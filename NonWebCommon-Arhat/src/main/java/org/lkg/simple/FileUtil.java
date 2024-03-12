d094a6b505cb329a915febd64849de343ac1d1db
李开广 <likaiguang@songguo7.com>
2024-03-12 15:50:51 +0800

    统一使用io.github前缀

--- NonWebCommon-Arhat/src/main/java/org/lkg/simple/FileUtil.java
+++ NonWebCommon-Arhat/src/main/java/org/lkg/simple/FileUtil.java
@@ -1,20 +1,15 @@
 package org.lkg.simple;
 
-import org.springframework.util.ObjectUtils;
-
 import java.io.*;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * @description:
- * @author: 李开广
- * @date: 2023/6/1 4:17 PM
+ * description:
+ * author: 李开广
+ * date: 2023/6/1 4:17 PM
  */
 public class FileUtil {
     public static String readFile(String url) throws IOException, URISyntaxException {
@@ -77,7 +72,7 @@ public class FileUtil {
 
     public static void main(String[] args) {
         try {
-            System.out.println(readFile("Common-Arhat/src/main/java/org/lkg/simple/a.txt"));
+            System.out.println(readFile("Common-Arhat/src/main/java/org.lkg/simple/a.txt"));
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
