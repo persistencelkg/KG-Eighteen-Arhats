78875c6b4e640b45d0db842c92920f0698650251
likaiguang <mac@fuchen.local>
2024-07-08 00:17:58 +0800

    1. 通用化es建立索引v1

--- NoSql-Arhat/src/main/java/org/lkg/elastic_search/crud/EsMetaApIServiceImpl.java
+++ NoSql-Arhat/src/main/java/org/lkg/elastic_search/crud/EsMetaApIServiceImpl.java
@@ -57,6 +57,7 @@ public class EsMetaApIServiceImpl implements EsMetaApIService<Orders> {
             for (java.lang.reflect.Field field : indexClass.getDeclaredFields()) {
                 mappingBuilder.startObject(ObjectUtil.camelToUnderline(field.getName()));
                 // 如何支持keyword
+
                 Class<?> fieldType = field.getType();
                 String esType = EsFieldType.getEsType(fieldType);
                 ObjectNode jsonNodes = new ObjectNode(JsonNodeFactory.instance);
