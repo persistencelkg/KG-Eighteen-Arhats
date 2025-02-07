3245c0e492eaa8ff37c5996ff54b725254655967
likaiguang <mac@fuchen.local>
2025-02-07 23:16:53 +0800

    暂存

--- DataCache-Arhat/src/main/java/org/lkg/cache/distrubuition/RedisTopicClearCache.java
+++ DataCache-Arhat/src/main/java/org/lkg/cache/distrubuition/RedisTopicClearCache.java
@@ -1,9 +1,18 @@
 package org.lkg.cache.distrubuition;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
 /**
  * Description:
  * Author: 李开广
  * Date: 2024/9/18 7:56 PM
  */
+@Component
 public class RedisTopicClearCache {
+
+
+    // 1. 发送redis # topic
+
 }
